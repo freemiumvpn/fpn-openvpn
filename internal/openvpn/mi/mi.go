13ac842634f6098e61319d9ae78084c52a1b72b2
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2021-02-03 22:39:40 +0000

    chore(mi): initial wrapper (#3)
    * chore(mi): initial wrapper
    
    * chore(mi): initial wrapper

---
+++ internal/openvpn/mi/mi.go
@@ -0,0 +1,69 @@
+package mi
+
+import (
+	"fmt"
+	"net"
+)
+
+/**
+mi stands for Management Interface
+*/
+
+const (
+	outputSuccess = "SUCCESS: "
+	outputError   = "ERROR: "
+	outputEnd     = "END"
+)
+
+type (
+	// ManagementInterface structure
+	ManagementInterface struct {
+		client  net.Conn
+		replies <-chan []byte
+		events  <-chan []byte
+	}
+)
+
+// New instantiates a management interface
+func New(port string) (*ManagementInterface, error) {
+	conn, err := net.Dial("tcp", port) // TODO: tcp vs unix
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: conn.SetWriteDeadline(time.Time{})
+
+	replies := make(chan []byte, 2)
+	events := make(chan []byte, 2)
+
+	go CreateConnectionListener(conn, events, replies)
+
+	return &ManagementInterface{
+		client:  conn,
+		events:  events,
+		replies: replies,
+	}, nil
+}
+
+// GetHelp maps to help
+func (mi *ManagementInterface) GetHelp() ([]byte, error) {
+	return mi.write([]byte("help \n"))
+}
+
+func (mi *ManagementInterface) write(cmd []byte) ([]byte, error) {
+	_, err := mi.client.Write(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	return mi.read()
+}
+
+func (mi *ManagementInterface) read() ([]byte, error) {
+	reply, ok := <-mi.replies
+	if !ok {
+		return nil, fmt.Errorf("Reply channel closed while awaiting")
+	}
+
+	return reply, nil
+}
