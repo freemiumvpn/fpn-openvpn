8f8872cd73feabcbdfa942570eb08d56ce5576d4
Julian <juliantellezmendez@gmail.com>
2021-02-03 20:21:48 +0000

    chore: initial cli

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+const (
+	appName        = "fpn-openvpn"
+	appDescription = "openvpn wrapper"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = appName
+	app.Description = appDescription
+	app.Action = appAction
+
+	err := app.Run(os.Args)
+	if err != nil {
+		logrus.WithError(err).Fatal("Failed to run")
+	}
+}
+
+func appAction(ctx *cli.Context) error {
+	return nil
 }
