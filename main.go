c6ab988bfa08988d097b764b901da15cf3d4eb84
Julian <juliantellezmendez@gmail.com>
2021-02-06 17:05:46 +0000

    chore(grpc): create initial server

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"os"
 
+	"github.com/freemiumvpn/fpn-openvpn-server/generated/vpn"
+	"github.com/freemiumvpn/fpn-openvpn-server/internal/grpc"
 	"github.com/freemiumvpn/fpn-openvpn-server/internal/openvpn/mi"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -20,6 +23,12 @@ var (
 			Usage:   "Management Interface Port",
 			EnvVars: []string{"MANAGEMENT_INTERFACE_PORT"},
 			Value:   ":5555",
+		},
+		&cli.StringFlag{
+			Name:    "grpc-port",
+			Usage:   "GRPC port",
+			EnvVars: []string{"GRPC_PORT"},
+			Value:   ":8989",
 		},
 	}
 )
@@ -37,18 +46,32 @@ func main() {
 	}
 }
 
-func appAction(ctx *cli.Context) error {
-	port := ctx.String("management-interface-port")
-	managementInterface, err := mi.New(port)
-	if err != nil {
-		return err
-	}
+func appAction(cliCtx *cli.Context) error {
+	port := cliCtx.String("management-interface-port")
 
-	reply, _ := managementInterface.GetHelp()
-	println(string(reply))
+	ctx := cliCtx.Context
+	eg, ctx := errgroup.WithContext(ctx)
 
-	reply, _ = managementInterface.GetStats()
-	println(string(reply))
+	grpcServer := grpc.New(ctx, grpc.Options{
+		Address: cliCtx.String("grpc-port"),
+	})
 
-	return nil
+	eg.Go(func() error {
+		return grpcServer.Listen(ctx, &vpn.UnimplementedVpnServiceServer{})
+	})
+
+	eg.Go(func() error {
+		managementInterface, err := mi.New(port)
+		if err != nil {
+			return err
+		}
+		reply, _ := managementInterface.GetHelp()
+		println(string(reply))
+
+		reply, _ = managementInterface.GetStats()
+		println(string(reply))
+		return nil
+	})
+
+	return eg.Wait()
 }
